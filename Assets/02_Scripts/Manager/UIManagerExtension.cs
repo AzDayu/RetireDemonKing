cbcd4605df41fdef6c46d0bc746e765d47ea7bc4
조윤상 <jogreenx21@gmail.com>
2026-08-10 15:02:38 +0900

    add : ui 매니저 스크립트 추가

---
+++ Assets/02_Scripts/Manager/UIManagerExtension.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public enum UIRootType
+{
+    None = 0,
+    BackGroundUI,
+    MainUI,
+    ContentUI,
+    PopupUI,
+    VeryFrontUI
+}
+
+public enum UIType
+{
+    StartTitleUI,
+    LoadingUI,
+}
+
+public static class UIManagerExtension
+{
+    public static string GetUIPath(this UIManager uiManager, UIRootType uiRootType, UIType uiType)
+    {
+        string path = string.Empty;
+
+        path = $"UIPrefabs/{uiRootType}/{uiType}";
+        return path;
+    }
+
+    public static void ShowStartupUIOnGameStart(this UIManager uiManager)
+    {
+        uiManager.OpenUI(UIRootType.MainUI, UIType.StartTitleUI);
+    }
+
+
+    public static void OpenLoadingUI(this UIManager uiManager)
+    {
+        var uiBase = uiManager.OpenUI(UIRootType.VeryFrontUI, UIType.LoadingUI);
+        if (uiBase == null)
+        {
+            Debug.LogWarning("UI가 생성되지 않았습니다");
+            return;
+        }
+    }
+    public static void CloseLoadingUI(this UIManager uiManager)
+    {
+        uiManager.CloseUI(UIRootType.VeryFrontUI, UIType.LoadingUI);
+    }
+
+
+}
